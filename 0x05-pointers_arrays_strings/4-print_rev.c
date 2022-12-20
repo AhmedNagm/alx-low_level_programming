2db73fea84ce69ecc7aa2b5141ba370a5041a817
root <ahmed-nagm@outlook.com>
2022-12-19 16:40:41 -0800

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,18 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i;
 
-	while (s[index++])
-		len++;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	
+	i++;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
-	_putchar('\n');
+	while (s[i] != '\0')
+	{
+		_putchar(s[i--]);
+	}
+	_putchar('n');
 }
-
