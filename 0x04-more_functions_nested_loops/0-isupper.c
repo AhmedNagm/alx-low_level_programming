14d6b5a7cae07244eb4d17d1614f774a866ac1d6
Ahmed Nagm <ahmed-nagm@outlook.com>
2023-01-12 11:01:00 +0200

    is upper case file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,19 @@
-#include "main.h"
-
-/**
- * _isupper - uppercase letters
- * @c: character to check
- *
- * Return: 0 or 1
+/*
+ * File: 0-isupper.c
+ *Auth: Ahmed Nagm
  */
 
+#include "main.h"
+
+/*
+ *_isupper - check if the input is upper case or not.
+ *@c: the charcter in question.
+ *Return: 1 if upper 0 otherwise.
+ */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
 	else
 		return (0);
