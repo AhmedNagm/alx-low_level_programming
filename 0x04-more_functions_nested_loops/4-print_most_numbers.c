f48234f0e4f9c36f918030fb266ac266161df6cb
Ahmed Nagm <ahmed-nagm@outlook.com>
2023-01-20 00:27:23 +0200

    fixxed comments for ms betty

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,7 +5,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints
+ * print_most_numbers - prints the numbers from 1-9 but not 2 & 4
  *
  * Return: void
  */
@@ -13,12 +13,13 @@
 void print_most_numbers(void)
 {
 	int i;
+	
 	for (i = 0; i <=9; i++)
 	{
 		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
 		}
-		}
+	}
 		_putchar('\n');
 }
