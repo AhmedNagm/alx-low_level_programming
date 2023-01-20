f5efc79fb7270392a5224394107783deaed02ee1
Ahmed Nagm <ahmed-nagm@outlook.com>
2023-01-20 04:11:08 +0200

    more functions and loops no 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,29 @@
+/*
+ * File: 5-more_numbers.c
+ * Auth: Ahmed Nagm
+ */
+
 #include "main.h"
 
 /**
- * more_numbers - nmbr fct
+ * more_numbers - prints numbers from 1 - 14, 10 times
  *
- * Return: no return
+ * Return: void
  */
 
 void more_numbers(void)
 {
+	int i, count;
 
-int i, j;
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (count = 0; count < 15; count++)
 		{
-			if (j >= 10)
+			if (count >= 10)
 			_putchar('1');
-			_putchar(j % 10 + '0');
+			_putchar((count % 10) + '0');
 		}
 			_putchar('\n');
-		}
+	}
+	
 }
