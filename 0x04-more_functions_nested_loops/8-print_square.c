417fdc91c179a3df178bbfaaae2133f7e61d0fcf
AhmedNagm <ahmed-nagm@outlook.com>
2023-01-29 20:13:07 +0200

    more functions and loops no 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,30 @@
+/*
+ * File: 8-print_squre.c
+ * Auth: Ahmed Nagm
+ */
+
 #include "main.h"
 
 /**
- * print_square - prints a square of size size
- * @size: size of the square
- *
- * Return: void
- */
-
+ * print_square - prints the # sign to build a square.
+ * @size: the size of the length and width of a squre.
+ * Reture: void.
+*/
 
 void print_square(int size)
 {
-	int i, j;
+    int i, j;
 
-	if (size <= 0)
-		_putchar('\n');
-
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < (size); j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
-	}
-
+    if (size <= 0)
+        _putchar('\n');
+    
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j <= size; j++)
+        {
+            _putchar('#');
+        }
+        _putchar('#');
+        _putchar('\n');
+    }
 }
