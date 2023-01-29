95b95d902a5011f205bf890901247e4401894f34
AhmedNagm <ahmed-nagm@outlook.com>
2023-01-29 20:29:59 +0200

    more functions and loops no 8 fixing spaces with vs

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,20 +11,24 @@
  * Reture: void.
 */
 
+/*
+This function prints a square with # signs.
+It takes one int parameter size which defines the size of the square.
+If size is <= 0, the function simply prints a new line.
+*/
 void print_square(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-        _putchar('\n');
-    
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j <= size; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('#');
-        _putchar('\n');
-    }
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+		_putchar('#'); 
+		}
+		_putchar('\n');
+	}
 }
