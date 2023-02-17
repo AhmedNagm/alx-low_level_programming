c41ec3a7e1d1ff40f003c1255ff1561b00d704c1
AhmedNagm <ahmed-nagm@outlook.com>
2023-02-17 16:57:07 +0200

    Fixing

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,17 @@
 /*
-* File: 0-reset_to_98.c
-* Auth: Ahmed Nagm 
-* For: ALX
-*/
+ * File: 0-reset_to_98.c
+ * Auth: Ahmed Nagm
+ * For: ALX
+ */
 
 #include "main.h"
 
 /**
  * reset_to_98 - resets the value of pointer to 98.
  * @n: pointer to be reset to 98.
- * 
+ *
  * Return: void.
-*/
-
+ */
 
 void reset_to_98(int *n)
 {
