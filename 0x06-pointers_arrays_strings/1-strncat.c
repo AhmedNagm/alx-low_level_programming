26f01e1650e85dad52bc5adf787c29618ff2e155
AhmedNagm <ahmed-nagm@outlook.com>
2023-03-01 23:35:16 +0200

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,5 @@
 /*
- * File: 0-strcat.c
+ * File: 1-strncat.c
  * Auth: Ahmed Nagm
  * Project: ALx
  */
@@ -7,26 +7,26 @@
 #include "main.h"
 
 /**
- * _strcat - concats two strings
+ * _strncat - concats two strings with a specific range
  * @dest: String to add to.
  * @src: String to add from.
- *
+ * @n: range of copy.
  * Return: dest.
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
-	for (j = 0; src[j] != '\0'; j++)
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i] = src[j];
 		i++;
 	}
-	dest[i] = '\0';
 
+	dest[i] = '\0';
 	return (dest);
 }
